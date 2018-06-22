92565872d630c18bb071072c0326d467c71cef43
Niek van Losenoord <niek_losenoord@hotmail.com>
2018-06-22 15:36:59 +0000

    3dwd first

--- js/contact_me.js
+++ js/contact_me.js
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", het lijkt erop dat mijn mail server niet reageert, je kunt een ogenblik wachten of mij contacten op niek_losenoord@hotmail.com"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
